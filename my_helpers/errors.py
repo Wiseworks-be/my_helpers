9640fdb53f5c40d5b2ba029f0376f854e5beae86
rayo254254 <r1019767@student.thomasmore.be>
2025-09-02 15:44:42 +0200

    added the log error function

--- my_helpers/errors.py
+++ my_helpers/errors.py
@@ -1,7 +1,24 @@
 """Error handling utilities."""
 import logging
 
+# Logger setup
+def setup_logger(name, level="INFO"):
+    """Setup a basic logger."""
+    logger = logging.getLogger(name)
+    logger.setLevel(getattr(logging, level))
 
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
+
+def log_error(message, logger_name="my-helpers"):
+    """Quick error logging function."""
+    logger = setup_logger(logger_name)
+    logger.error(message)
 # webhook
 
 def check_mandatory_args(args: dict):
