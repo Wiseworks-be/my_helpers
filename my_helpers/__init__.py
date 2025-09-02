0ae60973e5a48f72ac3cb27dddf0283be6846b73
rayo254254 <r1019767@student.thomasmore.be>
2025-09-02 15:05:44 +0200

    added helper functions for 15a billit webhooks

--- my_helpers/__init__.py
+++ my_helpers/__init__.py
@@ -1,7 +1,8 @@
 """Reusable helper functions for AppSheet, Billit, and other integrations."""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # Import functions to make them easily accessible
-from .errors import log_error, setup_logger
-from .appsheet import format_appsheet_data
+from .errors import log_error, setup_logger, check_mandatory_args
+from .appsheet import format_appsheet_data, get_appsheet_url, post_data_to_appsheet
 from .billit import format_invoice_data
+from .notifications import send_push_notification
