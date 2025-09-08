fdc1c5d33ef8b31dc449a93bc261b27b1b4f9e7b
rayo254254 <r1019767@student.thomasmore.be>
2025-09-08 16:48:03 +0200

    .

--- my_helpers/__init__.py
+++ my_helpers/__init__.py
@@ -2,7 +2,7 @@
 __version__ = "0.1.1"
 
 # Import functions to make them easily accessible
-from .errors import log_error, setup_logger, check_mandatory_args
-from .appsheet import  get_appsheet_url, post_data_to_appsheet
+from my_helpers.errors import log_error, setup_logger, check_mandatory_args
+from my_helpers.appsheet import  get_appsheet_url, post_data_to_appsheet
 
-from .notifications import send_push_notification
+from my_helpers.notifications import send_push_notification
