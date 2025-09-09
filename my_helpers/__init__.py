760d7f731f4424495beec5ebc686df0c8b876997
rayo254254 <r1019767@student.thomasmore.be>
2025-09-09 13:25:50 +0200

    ..

--- my_helpers/__init__.py
+++ my_helpers/__init__.py
@@ -6,7 +6,7 @@ from my_helpers.errors import log_error, setup_logger, check_mandatory_args
 from my_helpers.appsheet import  get_appsheet_url, post_data_to_appsheet
 from my_helpers.billit_utils_1 import  fetch_billit_file
 from my_helpers.notifications import send_push_notification
-from my_helpers.webhook_utils import v1, v2
+
 from my_helpers.exceptions import (
     ExternalAPIError,
     MethodNotAllowedError,
